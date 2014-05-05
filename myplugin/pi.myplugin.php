9b957db154105b2f34c3becb56552c052a909374
Bradley Flood <bradley.flood@gmail.com>
2014-05-05 13:20:18 +1000

    Basic layout of base plugin

---
+++ myplugin/pi.myplugin.php
@@ -0,0 +1,49 @@
+<?php
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$plugin_info = array(
+	'pi_name'        => 'MyPlugin',
+	'pi_version'     => '1.0.0',
+	'pi_author'      => 'Author Name',
+	'pi_author_url'  => 'http://www.flood7.com/',
+	'pi_description' => 'Description of my plugin',
+	'pi_usage'       => Myplugin::usage()
+  );
+
+
+class Myplugin
+{
+
+	public $return_data = ""; 
+
+  public function Myplugin()
+  {
+    $this->EE =& get_instance();
+    $this->EE->load->helper('string');
+  }
+
+
+	/**
+	 * Usage
+	 * This function describes how the plugin is used.
+	 * @access	public
+	 * @return	string
+	 */
+	
+  public function usage()
+  {
+	  ob_start(); 
+	  ?>
+	  
+	  	<p>Instructions for using MyPlugin</p>
+
+	  <?php
+	  $buffer = ob_get_contents();
+		
+	  ob_end_clean(); 
+
+	  return $buffer;
+  }
+
+}
