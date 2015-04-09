6f0b3b351ed8b2eef05215ff7893561f42bbbd04
Bradley Flood <bradleyflood@users.noreply.github.com>
2015-04-09 15:39:06 +1000

    Update pi.myplugin.php

--- myplugin/pi.myplugin.php
+++ myplugin/pi.myplugin.php
@@ -3,57 +3,66 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $plugin_info = array(
-	'pi_name'        => 'MyPlugin',
-	'pi_version'     => '1.0.0',
-	'pi_author'      => 'Author Name',
-	'pi_author_url'  => 'http://www.flood7.com/',
-	'pi_description' => 'Description of my plugin',
-	'pi_usage'       => Myplugin::usage()
-  );
+    'pi_name'        => 'MyPlugin',
+    'pi_version'     => '1.0.0',
+    'pi_author'      => 'Bradley Flood',
+    'pi_author_url'  => 'http://www.bradleyflood.com/',
+    'pi_description' => 'Description of my plugin',
+    'pi_usage'       => Myplugin::usage()
+);
 
 
 class Myplugin
 {
 
-	public $return_data = ""; 
-
-  public function Myplugin()
-  {
-    $this->EE =& get_instance();
-  }
-  
-  /**
-   * Usage
-   * This function describes how the plugin is used.
-   * @access  public
-   * @return  string
-   */
-  public function hello()
-  {
-    return 'Hello world!'; // {exp:myplugin:hello}
-  }
 
 
-	/**
-	 * Usage
-	 * This function describes how the plugin is used.
-	 * @access	public
-	 * @return	string
-	 */
-	
-  public function usage()
-  {
-	  ob_start(); 
-	  ?>
-	  
-	  	<p>Instructions for using MyPlugin</p>
 
-	  <?php
-	  $buffer = ob_get_contents();
-		
-	  ob_end_clean(); 
+    public function Myplugin()
+    {
+        $this->EE =& get_instance();
+    }
 
-	  return $buffer;
-  }
+
+
+
+
+    /**
+     * HELLO
+    */
+    public function hello()
+    {
+        // Grab template tag parameter
+        $string = ee()->TMPL->fetch_param('string');
+        
+        // {exp:myplugin:hello string="Hello World!"}
+        return $string;
+    }
+
+
+
+
+
+    /**
+    * Usage - EE Control Panel
+    */
+    public function usage()
+    {
+        ob_start(); 
+        ?>
+
+        MyPlugin Instructions
+
+        <?php
+        $buffer = ob_get_contents();
+
+        ob_end_clean(); 
+
+        return $buffer;
+    }
+
+
+
+
 
 }
